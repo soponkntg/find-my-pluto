1b7ecfa6d8986e7bae6547e7a5da8571ed1eb9ea
soponkntg <sopon-1231@hotmail.com>
2023-04-06 16:37:59 +0700

    carousel

--- src/pages/found-dog.tsx
+++ src/pages/found-dog.tsx
@@ -1,27 +1,7 @@
 import { PageLayout } from "@/component";
-import { Carousel } from "react-responsive-carousel";
-import dog from "../../public/dog.png";
-import Image from "next/image";
 
 const FoundDog = () => {
-  return (
-    <PageLayout>
-      <div>
-        <Carousel>
-          <div
-            className={`w-[300px] h-[400px] rounded-t-xl overflow-hidden relative object-contain ${"rounded-b-xl"}`}
-          >
-            <Image src={dog} alt="dog" fill />
-          </div>
-          <div
-            className={`w-[300px] h-[400px] rounded-t-xl overflow-hidden relative object-contain ${"rounded-b-xl"}`}
-          >
-            <Image src={dog} alt="dog" fill />
-          </div>
-        </Carousel>
-      </div>
-    </PageLayout>
-  );
+  return <PageLayout>asd</PageLayout>;
 };
 
 export default FoundDog;
